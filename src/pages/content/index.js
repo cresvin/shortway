f536f9cbbca6804d7600e78a47c3208c68eb8876
cresvin <cresvinn@gmail.com>
2024-01-29 18:03:30 +0100

    supports for inputs

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -1,27 +1,32 @@
 const textareas = Array.from(document.querySelectorAll("textarea"));
+const inputs = Array.from(document.querySelectorAll("input[type='text']"));
 
 let shortcuts = [];
 
 async function getShortcuts() {
   const result = await chrome.storage.local.get("shortcuts");
   shortcuts = result.shortcuts || [];
-  console.log(result);
 }
-
 getShortcuts();
 
+const handleShortcuts = (element) => {
+  const value = element.value;
+
+  const shortcut = shortcuts.find((shortcut) =>
+    value.includes(shortcut.prefix)
+  );
+
+  if (shortcut) {
+    const regex = new RegExp(shortcut.prefix, "g");
+    const cleaned = value.replace(regex, `${shortcut.content} `);
+    element.value = cleaned;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => handleShortcuts(input));
+});
+
 textareas.forEach((textarea) => {
-  textarea.addEventListener("input", () => {
-    const value = textarea.value;
-
-    const shortcut = shortcuts.find((shortcut) =>
-      value.includes(shortcut.prefix)
-    );
-
-    if (shortcut) {
-      const regex = new RegExp(shortcut.prefix, "g");
-      const cleaned = value.replace(regex, `${shortcut.content} `);
-      textarea.value = cleaned;
-    }
-  });
+  textarea.addEventListener("input", () => handleShortcuts(textarea));
 });
